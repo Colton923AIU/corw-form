de1b929ba4cf3ae129724768b03efedde0ef83bd
Colton923AIU <colton923aiu@gmail.com>
2024-10-15 11:01:22 -0500

    .

--- src/webparts/cwform/components/Cwform.tsx
+++ src/webparts/cwform/components/Cwform.tsx
@@ -67,6 +67,7 @@ const Cwform: React.FC<ICwformWebPartProps> = ({
   formList,
   spHttpClient,
 }) => {
+  const [submitted, setSubmitted] = React.useState<boolean>(false);
   const userData = useData({
     absoluteUrl: absoluteUrl,
     spHttpClient: spHttpClient,
@@ -92,170 +93,174 @@ const Cwform: React.FC<ICwformWebPartProps> = ({
   return (
     <section className={styles.cwform}>
       <h2>Cancel / Withdrawal Form</h2>
-      <form
-        onSubmit={handleSubmit(async data => {
-          if (!userData) return;
-          const CDOA = userData.filter(item => {
-            if (item.CDOA.Id === parseInt(data.CDOA)) {
-              return true;
-            }
-          })[0].CDOA;
-
-          const DSM = userData.filter(item => {
-            if (item.DSM.Title === data.DSM) {
-              return true;
-            }
-          })[0].DSM;
-          const validData: any = data;
-          validData.CDOANameId = CDOA.Id;
-          validData.CDSMId = DSM.Id;
-          validData.StudentID = parseInt(data.StudentID);
-          validData.AA_x002f_FAAdvisorId = await getUserIdByemail({
-            spHttpClient: spHttpClient,
-            email: data.AA_x002f_FAAdvisor[0].secondaryText,
-            url: cdoaToDSMListURL,
-          }).then(data => {
-            return data.Id;
-          });
-
-          delete validData.CDOA;
-          delete validData.DSM;
-          delete validData.AA_x002f_FAAdvisor;
-
-          console.log('validData: ', validData);
-          reset();
-          spHttpClient
-            .post(formList, SPHttpClient.configurations.v1, {
-              body: JSON.stringify(validData),
-            })
-            .then((response: any) => {
-              if (!response.ok) {
-                return response.json().then((err: any) => {
-                  throw new Error(JSON.stringify(err));
-                });
-              }
-              return response.json();
-            })
-            .then((data: any) => {
-              console.log('Success:', data);
-            })
-            .catch((error: any) => {
-              console.log('Fail:', error);
-            });
-        })}
-      >
-        <ControlledDropdown
-          errorMessage={errors.CorW?.message}
-          control={control}
-          name="CorW"
-          label="Request Type"
-          options={[
-            { key: 'Cancel', text: 'Cancel' },
-            { key: 'Withdrawal', text: 'Withdrawal' },
-          ]}
-          onChange={option => {
-            setValue('CorW', option);
-          }}
-        />
-        <ControlledTextField
-          errorMessage={errors.StudentName?.message}
-          control={control}
-          name="StudentName"
-          label="Student Name"
-        />
-        <ControlledTextField
-          errorMessage={errors.StudentID?.message}
-          control={control}
-          name="StudentID"
-          label="Student ID"
-          type="number"
-        />
-        <ControlledDatePicker
-          control={control}
-          name="StartDate"
-          label="Current Start Date"
-        />
-        {/* {corwState ? ( */}
-        {watch('CorW') === 'Withdrawal' ? (
-          <>
-            <ControlledTextField
-              errorMessage={errors.Notes?.message}
-              control={control}
-              name="Notes"
-              label="Student's Exact Written Request"
-              type="text"
-            />
-            <ControlledDropdown
-              errorMessage={errors.DocumentedInNotes?.message}
-              control={control}
-              name="DocumentedInNotes"
-              label="Documented in Notes"
-              options={[
-                { key: 'yes', text: 'Yes' },
-                { key: 'no', text: 'No' },
-              ]}
-            />
-            <ControlledTextField
-              errorMessage={errors.InstructorName?.message}
-              control={control}
-              name="InstructorName"
-              label="Instructor Name"
-              type="text"
-            />
-            <ControlledDropdown
-              errorMessage={errors.ESA?.message}
-              control={control}
-              name="ESA"
-              label="ESA"
-              options={[
-                { key: 'yes', text: 'Yes' },
-                { key: 'no', text: 'No' },
-              ]}
-            />
-          </>
-        ) : null}
-        <ControlledPeoplePicker
-          errorMessage={errors.AA_x002f_FAAdvisor?.message}
-          control={control}
-          name="AA_x002f_FAAdvisor"
-          context={context}
-          titleText="Financial Aid Advisor (AA or FA to be notified)"
-          personSelectionLimit={1}
-          disabled={false}
-          searchTextLimit={5}
-        />
-        <ControlledDropdown
-          errorMessage={errors.CDOA?.message}
-          control={control}
-          name="CDOA"
-          label="CDOA Name"
-          options={userData.map(item => ({
-            key: item.CDOA.Id.toString(),
-            text: item.CDOA.Title,
-          }))}
-          onChange={val => {
-            const DSMValue = userData?.filter(item => {
-              if (item.CDOA.Id === parseInt(val)) {
+      {submitted ? (
+        <h2>Form Submitted</h2>
+      ) : (
+        <form
+          onSubmit={handleSubmit(async data => {
+            if (!userData) return;
+            const CDOA = userData.filter(item => {
+              if (item.CDOA.Id === parseInt(data.CDOA)) {
                 return true;
               }
-            })[0].DSM.Title;
-            setValue('DSM', DSMValue);
-          }}
-        />
-        <ControlledTextField
-          errorMessage={errors.DSM?.message}
-          control={control}
-          name="DSM"
-          label="DSM"
-          type="text"
-          disabled={true} // Set to true or false based on your requirements
-        />
-        <PrimaryButton
-          type="submit"
-          text="Submit"
-          style={{ marginTop: '5px' }}
-        />
-      </form>
+            })[0].CDOA;
+
+            const DSM = userData.filter(item => {
+              if (item.DSM.Title === data.DSM) {
+                return true;
+              }
+            })[0].DSM;
+            const validData: any = data;
+            validData.CDOANameId = CDOA.Id;
+            validData.CDSMId = DSM.Id;
+            validData.StudentID = parseInt(data.StudentID);
+            validData.AA_x002f_FAAdvisorId = await getUserIdByemail({
+              spHttpClient: spHttpClient,
+              email: data.AA_x002f_FAAdvisor[0].secondaryText,
+              url: cdoaToDSMListURL,
+            }).then(data => {
+              return data.Id;
+            });
+
+            delete validData.CDOA;
+            delete validData.DSM;
+            delete validData.AA_x002f_FAAdvisor;
+
+            reset();
+            setSubmitted(true);
+            spHttpClient
+              .post(formList, SPHttpClient.configurations.v1, {
+                body: JSON.stringify(validData),
+              })
+              .then((response: any) => {
+                if (!response.ok) {
+                  return response.json().then((err: any) => {
+                    throw new Error(JSON.stringify(err));
+                  });
+                }
+                return response.json();
+              })
+              .then((data: any) => {
+                console.log('Success:', data);
+              })
+              .catch((error: any) => {
+                console.log('Fail:', error);
+              });
+          })}
+        >
+          <ControlledDropdown
+            errorMessage={errors.CorW?.message}
+            control={control}
+            name="CorW"
+            label="Request Type"
+            options={[
+              { key: 'Cancel', text: 'Cancel' },
+              { key: 'Withdrawal', text: 'Withdrawal' },
+            ]}
+            onChange={option => {
+              setValue('CorW', option);
+            }}
+          />
+          <ControlledTextField
+            errorMessage={errors.StudentName?.message}
+            control={control}
+            name="StudentName"
+            label="Student Name"
+          />
+          <ControlledTextField
+            errorMessage={errors.StudentID?.message}
+            control={control}
+            name="StudentID"
+            label="Student ID"
+            type="number"
+          />
+          <ControlledDatePicker
+            control={control}
+            name="StartDate"
+            label="Current Start Date"
+          />
+          {/* {corwState ? ( */}
+          {watch('CorW') === 'Withdrawal' ? (
+            <>
+              <ControlledTextField
+                errorMessage={errors.Notes?.message}
+                control={control}
+                name="Notes"
+                label="Student's Exact Written Request"
+                type="text"
+              />
+              <ControlledDropdown
+                errorMessage={errors.DocumentedInNotes?.message}
+                control={control}
+                name="DocumentedInNotes"
+                label="Documented in Notes"
+                options={[
+                  { key: 'yes', text: 'Yes' },
+                  { key: 'no', text: 'No' },
+                ]}
+              />
+              <ControlledTextField
+                errorMessage={errors.InstructorName?.message}
+                control={control}
+                name="InstructorName"
+                label="Instructor Name"
+                type="text"
+              />
+              <ControlledDropdown
+                errorMessage={errors.ESA?.message}
+                control={control}
+                name="ESA"
+                label="ESA"
+                options={[
+                  { key: 'yes', text: 'Yes' },
+                  { key: 'no', text: 'No' },
+                ]}
+              />
+            </>
+          ) : null}
+          <ControlledPeoplePicker
+            errorMessage={errors.AA_x002f_FAAdvisor?.message}
+            control={control}
+            name="AA_x002f_FAAdvisor"
+            context={context}
+            titleText="Financial Aid Advisor (AA or FA to be notified)"
+            personSelectionLimit={1}
+            disabled={false}
+            searchTextLimit={5}
+          />
+          <ControlledDropdown
+            errorMessage={errors.CDOA?.message}
+            control={control}
+            name="CDOA"
+            label="CDOA Name"
+            options={userData.map(item => ({
+              key: item.CDOA.Id.toString(),
+              text: item.CDOA.Title,
+            }))}
+            onChange={val => {
+              const DSMValue = userData?.filter(item => {
+                if (item.CDOA.Id === parseInt(val)) {
+                  return true;
+                }
+              })[0].DSM.Title;
+              setValue('DSM', DSMValue);
+            }}
+          />
+          <ControlledTextField
+            errorMessage={errors.DSM?.message}
+            control={control}
+            name="DSM"
+            label="DSM"
+            type="text"
+            disabled={true} // Set to true or false based on your requirements
+          />
+          <PrimaryButton
+            type="submit"
+            text="Submit"
+            style={{ marginTop: '5px' }}
+          />
+        </form>
+      )}
     </section>
   );
 };
